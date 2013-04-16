45dd8d6ad77f3fdf1385997b049ba12a415e4a39
Stratis Konakas <konakas@hotmail.com>
2013-04-16 23:51:08 +0300

    Noaa parsing

--- src/assets/www/parsers/noaa.js
+++ src/assets/www/parsers/noaa.js
@@ -1,22 +1,6 @@
-function parseHnmsMetarPage(airport, html) {
-	var indexOfAirport = html.indexOf(airport);
-	var indexOfEquals = html.indexOf("=", indexOfAirport);
-	return html.substring(indexOfAirport, indexOfEquals);
-}
-
-function parseHnmsObservationPage(html) {
-	var start = -1;
-	var end = -1;
-	var result = [];
-	
-	while (true) {
-		start = html.indexOf("/hnms/greek/encodedinfo_popup_html?db_station=", end);
-		
-		if (start == -1) break;
-			
-		end = html.indexOf("'", start);
-		result.push(html.substring(start, end));
-	}
-	
-	return result;
+function parseNoaaMetarPage(xml) {
+	var start = "<raw_text>";
+	var indexOfStart = xml.indexOf(start);
+	var indexOfEnd = xml.indexOf("</raw_text>", indexOfStart);
+	return xml.substring(indexOfStart + start.length, indexOfEnd);
 }
