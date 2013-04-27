166092de9ca945a7c592f2df92a5013924cd05d0
Stratis Konakas <konakas@hotmail.com>
2013-04-27 12:14:46 +0300

    first real example

--- src/assets/www/main.js
+++ src/assets/www/main.js
@@ -3,5 +3,19 @@ function onLoad() {
 }
 
 function onDeviceReady() {
-	alert("ok");
+}
+
+function getLatestMetar() {
+	getLatestMetarFromNoaa();
+}
+
+function getLatestMetarFromHnms() {
+	var result = parseHnmsMetarPage("LGTT", input);
+}
+
+function getLatestMetarFromNoaa() {
+	$.get('http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString=LGAV', function(data) {
+		var result = parseNoaaMetarPage(data);
+		$('#result').html(result);
+		}, "text");
 }
