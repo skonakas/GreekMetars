d9d907c547ec77ccca17ea3ee029ab14acdc5ea3
Stratis Konakas <konakas@hotmail.com>
2013-08-15 15:31:07 +0300

    Added get all stations from HNMS

--- src/assets/www/parsers/hnms.js
+++ src/assets/www/parsers/hnms.js
@@ -20,3 +20,27 @@ function parseHnmsObservationPage(html) {
 	
 	return result;
 }
+
+var startText = "<option value='/hnms/greek/observation/observation_html?&dr_city=";
+function getAllStationsFromObservationPage(html) {
+	var start = -1;
+	var end = -1;
+	var middle = -1;
+	var results = [];
+	
+	while (true) {
+		start = html.indexOf(startText, end);
+		if (start == -1) break;
+			
+		middle = html.indexOf("'>", start);
+		end = html.indexOf("</option>", start);
+
+		var name = html.substring(start + startText.length, middle);
+		var text = html.substring(middle + 2, end);
+		
+		var result = { name: name, text: text };
+		results.push(result);
+	}
+	
+	return results;
+}
